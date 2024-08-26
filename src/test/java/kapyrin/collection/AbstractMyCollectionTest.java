82ffae3d91059ababf0d6a9f71260656c8ba5db0
Vladimir Kapyrine <vladimir.kapyrin@gmail.com>
2024-08-26 12:15:10 +0100

    little ad

--- src/test/java/kapyrin/collection/AbstractMyCollectionTest.java
+++ src/test/java/kapyrin/collection/AbstractMyCollectionTest.java
@@ -1,47 +1,32 @@
 package kapyrin.collection;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 abstract class AbstractMyCollectionTest<T> {
     protected CustomCollection<T> collection;
 
-    @BeforeEach
-   abstract void setUp();  // Абстрактный метод для инициализации коллекции
+    abstract void setUp();  // Абстрактный метод для инициализации коллекции
 
-    
-   abstract void testAdd();
+    abstract void testAdd();
 
-    
-   abstract void testContains();
+    abstract void testContains();
 
-    
-   abstract void testDefaultSizeIsZero();
+    abstract void testDefaultSizeIsZero();
 
-    
     abstract void testGet();
 
-    
     abstract void testSize();
 
-    
     abstract void testClear();
 
-    
     abstract void testRemoveByIndex();
 
-    
     abstract void testRemoveByElement();
 
-    
     abstract void testAddAll();
 
-    
     abstract void testSet();
 
-    
     abstract void testBubbleSort();
 
- 
     abstract void testConstructorWithAnotherCollectionToParameter();
 }
